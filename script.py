418794791c3715460c22913f97d5065b77e7e11b
gunnhildurf <gunnhildurf@gmail.com>
2015-05-10 18:11:58 +0000

    monkey ass

--- script.py
+++ script.py
@@ -4,19 +4,22 @@ def sortshow(show, dfolder):
     for root, dirs, files in os.walk(dfolder):
         for f in files:
             print(f)
-            if f == 'AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA.txt':
+            if f == 'testgaur.txt':
                 print("wooo")
             #TODO 1): finna nafnið í strengnum, há og lágstafa kombó, engin bil               
-                #show = show.replace(' ', '')
-                #s = os.path.join(dfolder, show)
-                #path = os.path.abspath(f)
-                #if not os.path.isdir(s):
-                 #   os.mkdir(s)
-                  #  os.rename(path, os.path.join(s, f))
-                   # print(s)
-                #else:
-                 #   pass
-                    # setja í s
+                show = show.replace(' ', '')
+                s = os.path.join(dfolder, show)
+            
+                path = os.path.abspath(os.path.join(root,f))
+                newpath = os.path.join(os.path.abspath(s),f)
+
+                if not os.path.isdir(s):
+                    os.mkdir(s)
+                    os.rename(path, newpath)
+                    print(newpath)
+                else:
+                    os.rename(path, newpath)
+                    print(newpath)
                 
                 #Setja allar skrár sem þetta passar við í eina möppu
                 #print(f)
@@ -24,11 +27,11 @@ def sortshow(show, dfolder):
 
 
 
-show = input('What do you feel like watching today? ')
-df = input('Where would you like to search for your shows? (type d for Downloads) \n Your search results will be stored in the folder you serched.')
-if df == 'd' or df == 'D':
-    dfolder = 'Downloads'
-else:
-    dfolder = df
+#show = input('What do you feel like watching today? ')
+#df = input('Where would you like to search for your shows? (type d for Downloads) \n Your search results will be stored in the folder you serched.')
+#if df == 'd' or df == 'D':
+#    dfolder = 'Downloads'
+#else:
+#    dfolder = df
 
-sortshow(show, dfolder)
+sortshow('house of cards', 'test')
